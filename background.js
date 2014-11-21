f88dadc53fa6d2c21db682aebb2647f6e3835a02
Silver Kuusik <silver.kuusik@gmail.com>
2014-11-21 22:46:38 +0100

    moved main.js to background.js so it runs only in background

--- background.js
+++ background.js
@@ -1,9 +1,90 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    id: "mainwin",
-    innerBounds: {
-      width: 640,
-      height: 480
+/* Found serial ports */
+var serialPorts = [];
+/* Opened serial port connection ID */
+var connectionId = -1;
+
+/* Logging messages */
+function log(msg) {
+    var msg_str = (typeof(msg) == 'object') ? JSON.stringify(msg) : msg;
+    console.log(msg_str);
+
+    /*var l = document.getElementById('log');
+    if (l) {
+        l.innerText += msg_str + '\n';
+    }*/
+}
+
+/* Interprets an ArrayBuffer as UTF-8 encoded string data. */
+var ab2str = function(buf) {
+    var bufView = new Uint8Array(buf);
+    var encodedString = String.fromCharCode.apply(null, bufView);
+    return decodeURIComponent(escape(encodedString));
+};
+
+/* Converts a string to UTF-8 encoding in a Uint8Array; returns the array buffer. */
+var str2ab = function(str) {
+    var encodedString = unescape(encodeURIComponent(str));
+    var bytes = new Uint8Array(encodedString.length);
+    for (var i = 0; i < encodedString.length; ++i) {
+        bytes[i] = encodedString.charCodeAt(i);
     }
-  });
-});
+    return bytes.buffer;
+};
+
+/* When serial */
+var onFinishedGettingDevices = function(foundSerialPorts) {
+    /* Push all found serial ports to the list */
+    for (var i = 0; i < foundSerialPorts.length; i++) {
+        serialPorts.push(foundSerialPorts[i].path);
+        log("Device " + i + ": " + foundSerialPorts[i].path);
+    }
+    /* When no serial ports found */
+    if (serialPorts.length == 0) {
+        log("No serial ports found");
+        /* TODO: send out external message to user */
+        return;
+    }
+    log("Connecting to serial ...");
+    /* Connect to the first serial port */
+    /* TODO: send out external message and let user choose port to connect to */
+    chrome.serial.connect(serialPorts[0], onConnectFinished);
+};
+
+/* When connecting to serial port finished */
+var onConnectFinished = function(connectionInfo) {
+    /* When conneting to serial port failed */
+    if (!connectionInfo) {
+        log("Connection failed");
+        /* TODO: send out external message to user */
+        return;
+    }
+    connectionId = connectionInfo.connectionId;
+    /* Successfully connected to the serial port */
+    log("Successfully connected to: " + connectionInfo.connectionInfo);
+}
+
+/* External messages from other extensions, apps or websites */
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+        log("Got message, sender url: " + sender.url + " request: " + request.action);
+        /* When connect to serial device */
+        if (request.action === "connect") {
+            log("Getting serial devices ...");
+            /* Get all serial devices */
+            chrome.serial.getDevices(onFinishedGettingDevices);
+            sendResponse({message: "starting to search for serial devices"});
+        /* When commands to send to serial device */
+        } else if (request.action === "commands") {
+            log("Received commands: " + request.commands);
+            if (connectionId != -1) {
+                chrome.serial.send(connectionId, str2ab(request.commands), function() {});
+                sendResponse({message: "successfully sent commands to serial device"});
+            } else {
+                log("Not connected to a serial device");
+                sendResponse({message: "not connected to a serial device"});
+            }
+        } else {
+            sendResponse({message: "unknown command"});
+        }
+    }
+);
